71dfa8c1453cd78ff3e81692e7540c9a008d6690
ll3me <i@l-3.cc>
2026-01-22 17:41:59 +0800

    feat: 添加关于页

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,22 @@
 export const SITE_TITLE = '日记岛';
-export const SITE_DESCRIPTION = '-';
+export const SITE_DESCRIPTION = '一个极简风格的博客记录空间。';
 export const SITE_ICP = '';
 export const SITE_MPS_BEIAN = '';
+
+export const SOCIALS = [
+    {
+        name: "Github",
+        url: "https://github.com/ll3me",
+        icon: "lucide:github",
+    },
+    {
+        name: "Twitter",
+        url: "",
+        icon: "lucide:twitter",
+    },
+    {
+        name: "Email",
+        url: "mailto:i@l-3.cc",
+        icon: "lucide:mail",
+    },
+];
